fc8851b7d74b2120f518e90884d7c02d51f961f7
jose <joselchecaexpo@gmail.com>
2022-01-20 18:56:07 +0100

    create basics for kick off plugin

---
+++ webdevtest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package WebDevTest
+ */
+/*
+Plugin Name: webdevtest
+Plugin URI: 
+Description: An amazing plugin
+Version: 1.0.0
+Author: WebDev
+Author URI:
+License: GPLv2 or later
+Text Domain: webdevtest
+*/
